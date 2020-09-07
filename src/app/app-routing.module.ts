c6d6c3a4c97ecfd8380930045e7f1f1afa7bb083
Luiz Rech <luizadre@hotmail.com>
2020-09-07 15:24:10 -0300

    consumindo API do IBGE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes:Routes = [
   {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
   {path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule)},
-  {path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)}
+  {path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(m => m.UsuarioModule)},
+  {path: 'estados', loadChildren: () => import('./estados/estados.module').then(m => m.EstadosModule)}
   // {path: '', pathMatch: 'full', redirectTo: '/home'}
 ];
 
